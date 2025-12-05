9d0dc02cb77beea9bbf134687f40f34c21a632d9
queencompbalam <33541771+queencompbalam@users.noreply.github.com>
2025-12-06 01:01:12 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "rigcompie-cache-v1";
+const urlsToCache = [
+  "/",
+  "/?m=1",
+  "/search",
+  "https://www.rigcompie.com/favicon.ico"
+];
+
+// Install SW
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch cache-first
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).then((fetchResponse) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        })
+      );
+    })
+  );
+});
+
+// Activate old cache cleanup
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
